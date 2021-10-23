8ef056a8695648bbf6bd14beed9ac0636aa9519e
David Palacio <dpalacio@growthaccelerationpartners.com>
2021-10-23 15:09:21 -0500

    added .net solution

---
+++ AspNetConsulIntegration/AspNetConsulIntegration/ConsulConfigProvider.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace AspNetConsulIntegration;
+public class EntityConfigurationSource : IConfigurationSource
+{
+    private readonly string _connectionString;
+
+    public EntityConfigurationSource(string connectionString) =>
+        _connectionString = connectionString;
+
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+
+        return new A();
+    }
+
+}
+
+public class A : IConfigurationProvider
+{
+    public IEnumerable<string> GetChildKeys(IEnumerable<string> earlierKeys, string parentPath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IChangeToken GetReloadToken()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Load()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Set(string key, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryGet(string key, out string value)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AspNetConsulIntegration/AspNetConsulIntegration/Program.cs
@@ -0,0 +1,37 @@
+using AspNetConsulIntegration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<A>(serviceProvider => new A());
+
+builder.Host.ConfigureAppConfiguration(configBuilder =>
+{
+    configBuilder.Sources.Clear();
+
+    configBuilder.AddJsonFile("", optional: true, reloadOnChange: true);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
